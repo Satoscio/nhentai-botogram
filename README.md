27742b0ef69b885be95f3c86b72ccc8298b565c6
Satoscio <satoscio65@gmail.com>
2020-05-08 19:06:39 +0200

    First Commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# god why do i do this
+
+Behold, the new nhentai bot, packed with a fuckton of new utilities
+that **you will never use because you're a functioning member of society
+and therefore you do not feel the urge to read hentai.**
+
+This bot uses exentio's nhentai API Python wrapper, so you'll also
+need to download his library to make this bot work
+
+## Update 2020-05-08
+
+### **FEATURES!**
+* **/random** - this command will generate a random ID between 1 and the
+  latest nhentai ID
+* **/latest** - do I need to tell you what does this command do?
+* **/search <ID>** - this command will search for a doujin with the ID you
+  put next to the command
+* **/info <ID>** - this command will give you detailed information about the
+  doujin ID you put next to the command such as cover image (**I am not
+  responsible if you get banned from a group because the cover image is NSFW**),
+  titles (short, english, japanese), number of pages, author, language and all tags.
+* **/start** - is this a feature?
+* **/github** - it's not like I want other people to see my code, b-baka!
+
+## **BIG THANKS TO**
+
+* [exentio](http://github.com/exentio), for making a wonderful Python library
+  for us to use :D @ https://github.com/exentio/nhentai_api
+* [Michele](https://github.com/xMicky24GIT), for helping me with the
+  Botogram framework (I'm a fucking dumbass aight)
+
+
+### *And remember...*
+
+![FUCK MIKO INO](https://i.imgur.com/hBUFGxw.jpg)
---
+++ main.py
@@ -0,0 +1,62 @@
+from random import randint
+from nhentai import Nhentai
+import botogram
+
+bot = botogram.create("BOT TOKEN")
+
+@bot.command("search")
+def search_command(chat, message, args):
+    if not args:
+        chat.send("Command usage: `/search <ID>`")
+    else:
+        try:
+            if Nhentai().book_info(int(args[0]))["title"]:
+                chat.send("https://nhentai.net/g/" + str(args[0]))
+        except KeyError:
+            chat.send("404 Not Found")
+        except ValueError:
+            chat.send("Invalid data")
+
+@bot.command("latest")
+def latest_command(chat, message, args):
+    chat.send("https://nhentai.net/g/" + str(Nhentai().latest_id()))
+
+@bot.command("random")
+def random_command(chat, message, args):
+    chat.send("https://nhentai.net/g/" + str(randint(1, Nhentai().latest_id())))
+
+@bot.command("info")
+def info_command(chat, message, args):
+    if not args:
+        chat.send("Command usage: `/info <ID>`")
+    else:
+        try:
+            if Nhentai().book_info(int(args[0]))["title"]:
+                dictx = Nhentai().book_info(int(args[0]))
+                chat.send_photo(url=Nhentai().book_cover(int(args[0])), caption=
+                                    "*Short title*: " + str(dictx["title"]["pretty"]) +
+                                    "\n*English title*: " + str(Nhentai().book_title(int(args[0]))) + 
+                                    "\n*Japanese title*: " + str(Nhentai().book_title_jp(int(args[0]))) +
+                                    "\n*Author*: " + str(Nhentai().book_artists(int(args[0]), return_string=True)) +
+                                    "\n*Language*: " + str(Nhentai().book_language(int(args[0]), return_string = True)) +
+                                    "\n*Pages*: " + str(Nhentai().book_pagenum(int(args[0]))) + 
+                                    "\n\n*Tags*: " + str(Nhentai().book_tags(int(args[0]), return_string = True)) + 
+                                    "\n\n*Read it here*: https://nhentai.net/g/" + str(int(args[0])), syntax="markdown")
+
+        except KeyError:
+            chat.send("404 Not Found")
+        except ValueError:
+            chat.send("Invalid data")
+
+@bot.command("start")
+def start_command(chat, message, args):
+    """WIP"""
+    chat.send("WIP")
+
+@bot.command("source")
+def source_command(chat, message, args):
+    """Bot source code on GitHub"""
+    chat.send("Bot Source: https://github.com/Satoscio/nhentai-rng-bot")
+
+if __name__ == "__main__":
+    bot.run()
